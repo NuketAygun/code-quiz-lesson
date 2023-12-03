a51f76286661a80848858fda72a3c71fcf62d2ee
Nuket <nukumoonday@gmail.com>
2023-12-03 22:34:23 +0000

    Questions have been prepared

---
+++ assets/js/question.js
@@ -0,0 +1,23 @@
+
+const questions = [
+    {
+        questionText: "Which is to declare a JavaScript variable?",
+        PotentialAnswers: ["var", "string","number"],
+        correctAnswer: "var"
+    },
+    {
+        questionText: "Which event for click an HTML element?",
+        PotentialAnswers: ["click","keydown","mouseover"],
+        correctAnswer: "click"
+    },
+    {
+        questionText: "Which methot is looping an array?",
+        PotentialAnswers: ["for","forEach","loop"],
+        correctAnswer: "forEach"
+    },
+    {
+        questionText: "Which method is merging two arrays?",
+        PotentialAnswers: ["contain","concat","add"],
+        correctAnswer: "concat"
+    }
+]
